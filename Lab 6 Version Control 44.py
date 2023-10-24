1c04efa064d27a409def07763d859f531227f892
blakemcgahee <148484976+blakemcgahee@users.noreply.github.com>
2023-10-24 18:05:44 -0400

    Update and rename Lab 6 Version Control 50.py to Lab 6 Version Control 44.py
    Updated Group Number

--- Lab 6 Version Control 44.py
+++ Lab 6 Version Control 44.py
@@ -1,29 +1,29 @@
 # Blake McGahee
 # Change Revision 1 - Removed dropped line on line 14.
 
+
+# Encoder function
 def encoder(password):
     result = ''
+    # 
     for digit in password:
         new_digit = str((int(digit) + 3) % 10)
         result += new_digit
     return result
 
+# Decoder function
 
-def decoder(password):
-    result = ''
-    for digit in password:
-        new_digit = str((int(digit) - 3) % 10)
-        result += new_digit
-    return result
-
-
+# Main function
 def main():
+    # Global variable to store value entered
     global value
+    # While loop for menu options
     while True:
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
         choice = input("Please enter an option: ")
+        # Menu options 1 - 3
         if choice == "1":
             value = input("Please enter your password to encode: ")
             print("Your password has been encoded and stored!")
@@ -31,10 +31,11 @@ def main():
             print("The encoded password is " + encoder(value) + ", and the original password is " + value)
         elif choice == "3":
             break
+            # Error catching for invalid input
         else:
-            print("Invalid selection")
+            print("Invalid input, please select 1 - 3")
         print()
 
-
+# Name function to call main function 
 if __name__ == '__main__':
     main()
